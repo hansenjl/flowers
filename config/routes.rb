b062b2ba0f03237726adb848625ffb593fb565b7
hansenjl <jenn.leigh.hansen@gmail.com>
2017-12-04 17:42:46 -0800

    Create event and fresh controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#home'
+  get '/about', to: 'welcome#about'
+  get '/testimonials', to: 'welcome#testimonial'
+
+
+  resources :fresh, only: [:show, :index]
+  resources :events, only: [:show, :index]
 end
